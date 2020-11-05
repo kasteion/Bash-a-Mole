c190d0290957961f23a261273a908004ef24c8d6
Fredy Augusto Castellón Bedoya <53954388+kasteion@users.noreply.github.com>
2020-11-05 15:20:12 -0600

    Delete 25_pbzip2.sh

--- BASH-SCRIPTING-COURSE/07-TAR-GZIP-PBZIP2-ZIP/25_pbzip2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Programa para ejemplificar el empaquetamiento con el comando pbzip2
-
-echo "Empaquetar todos los scripts de la carpeta ShellCourse"
-tar cvf shellcourse.tar *.sh
-pbzip2 -f shellcourse.tar
