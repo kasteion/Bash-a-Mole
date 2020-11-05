0a1a10a7879756a6b815e39fca5ee504df0ad965
Fredy Augusto Castellón Bedoya <53954388+kasteion@users.noreply.github.com>
2020-11-05 15:16:13 -0600

    Delete 11_download.sh

--- BASH-SCRIPTING-COURSE/04-DOWNLOAD/11_download.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Programa para ejemplificar el uso de la descarga de información desde internet
-# utilizando el comando wget
-
-echo "Descargar información de internet"
-wget https://downloads.apache.org/tomcat/tomcat-8/v8.5.56/bin/apache-tomcat-8.5.56.zip
